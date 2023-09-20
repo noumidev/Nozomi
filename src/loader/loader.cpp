52263ebff07f873776ccf4fd703c8e7057019e63
Nomi <nomiwen@outlook.de>
2023-09-20 02:38:18 +0200

    Add loader stubs

---
+++ src/loader/loader.cpp
@@ -0,0 +1,58 @@
+/*
+    Nozomi is an experimental HLE Switch emulator.
+    Copyright (C) 2023  noumidev
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "loader.hpp"
+
+#include <cstdio>
+#include <cstdlib>
+
+#include <plog/Log.h>
+
+#include "nro.hpp"
+
+namespace loader {
+
+void load(const char *path) {
+    PLOG_VERBOSE << "Input file: " << path;
+
+    FILE *file = std::fopen(path, "rb");
+
+    // Check if file exists
+    if (file == NULL) {
+        PLOG_FATAL << "Unable to open file " << path;
+
+        exit(0);
+    }
+
+    // Go through all supported file formats to find which loader to choose
+    if (nro::isNRO(file)) {
+        PLOG_VERBOSE << "Input file is NRO";
+
+        exit(0);
+    } else {
+        PLOG_FATAL << "Unrecognized executable format";
+
+        exit(0);
+    }
+
+    std::fclose(file);
+
+    exit(0);
+}
+
+}
