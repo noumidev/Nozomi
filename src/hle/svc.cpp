02b9eec7925d2b7d69d2bafff3c82458ddcb2a1b
Nomi <nomiwen@outlook.de>
2023-09-21 21:33:11 +0200

    Revamp handles, add svcConnectToNamedPort

--- src/hle/svc.cpp
+++ src/hle/svc.cpp
@@ -19,13 +19,16 @@
 #include "svc.hpp"
 
 #include <cstdlib>
+#include <cstring>
 #include <ios>
 
 #include <plog/Log.h>
 
 #include "cpu.hpp"
 #include "handle.hpp"
+#include "kernel.hpp"
 #include "memory.hpp"
+#include "object.hpp"
 #include "result.hpp"
 
 namespace hle::svc {
@@ -34,6 +37,7 @@ namespace SupervisorCall {
     enum : u32 {
         SetHeapSize = 0x01,
         QueryMemory = 0x06,
+        ConnectToNamedPort = 0x1F,
         GetInfo = 0x29,
     };
 }
@@ -62,6 +66,9 @@ void handleSVC(u32 svc) {
         case SupervisorCall::QueryMemory:
             svcQueryMemory();
             break;
+        case SupervisorCall::ConnectToNamedPort:
+            svcConnectToNamedPort();
+            break;
         case SupervisorCall::GetInfo:
             svcGetInfo();
             break;
@@ -72,81 +79,92 @@ void handleSVC(u32 svc) {
     }
 }
 
+void svcConnectToNamedPort() {
+    const u64 portName = sys::cpu::get(1);
+
+    PLOG_INFO << "svcConnectToNamedPort (port name* = " << std::hex << portName << ")";
+
+    const char *name = (char *)sys::memory::getPointer(portName);
+
+    sys::cpu::set(0, Result::Success);
+    sys::cpu::set(1, kernel::makeSession(kernel::getPort(name)->getHandle()).raw);
+}
+
 void svcGetInfo() {
     const u32 type = sys::cpu::get(1);
-    const Handle handle = sys::cpu::get(2);
+    const Handle handle = hle::makeHandle((u32)sys::cpu::get(2));
     const u64 subType = sys::cpu::get(3);
 
-    PLOG_INFO << "svcGetInfo (type = " << type << ", handle = " << std::hex << handle << ", sub type = " << subType << ")";
+    PLOG_INFO << "svcGetInfo (type = " << type << ", handle = " << std::hex << handle.raw << ", sub type = " << subType << ")";
 
     sys::cpu::set(0, Result::Success);
 
     switch (type) {
         case InfoType::AliasRegionAddress:
-            if ((handle != KernelHandles::CurrentProcess) || (subType != 0)) {
+            if ((handle.raw != KernelHandles::CurrentProcess) || (subType != 0)) {
                 PLOG_WARNING << "Unexpected handle/sub type for AliasRegionAddress";
             }
 
             sys::cpu::set(1, 0); // What is this?
             break;
         case InfoType::AliasRegionSize:
-            if ((handle != KernelHandles::CurrentProcess) || (subType != 0)) {
+            if ((handle.raw != KernelHandles::CurrentProcess) || (subType != 0)) {
                 PLOG_WARNING << "Unexpected handle/sub type for AliasRegionSize";
             }
 
             sys::cpu::set(1, 0); // What is this?
             break;
         case InfoType::HeapRegionAddress:
-            if ((handle != KernelHandles::CurrentProcess) || (subType != 0)) {
+            if ((handle.raw != KernelHandles::CurrentProcess) || (subType != 0)) {
                 PLOG_WARNING << "Unexpected handle/sub type for HeapRegionAddress";
             }
 
             sys::cpu::set(1, sys::memory::MemoryBase::Heap);
             break;
         case InfoType::HeapRegionSize:
-            if ((handle != KernelHandles::CurrentProcess) || (subType != 0)) {
+            if ((handle.raw != KernelHandles::CurrentProcess) || (subType != 0)) {
                 PLOG_WARNING << "Unexpected handle/sub type for HeapRegionSize";
             }
 
             sys::cpu::set(1, sys::memory::getHeapSize());
             break;
         case InfoType::TotalMemorySize:
-            if ((handle != KernelHandles::CurrentProcess) || (subType != 0)) {
+            if ((handle.raw != KernelHandles::CurrentProcess) || (subType != 0)) {
                 PLOG_WARNING << "Unexpected handle/sub type for TotalMemorySize";
             }
 
             sys::cpu::set(1, sys::memory::TOTAL_MEMORY_SIZE);
             break;
         case InfoType::UsedMemorySize:
-            if ((handle != KernelHandles::CurrentProcess) || (subType != 0)) {
+            if ((handle.raw != KernelHandles::CurrentProcess) || (subType != 0)) {
                 PLOG_WARNING << "Unexpected handle/sub type for UsedMemorySize";
             }
 
             sys::cpu::set(1, sys::memory::getUsedMemorySize());
             break;
         case InfoType::AslrRegionAddress:
-            if ((handle != KernelHandles::CurrentProcess) || (subType != 0)) {
+            if ((handle.raw != KernelHandles::CurrentProcess) || (subType != 0)) {
                 PLOG_WARNING << "Unexpected handle/sub type for AslrRegionAddress";
             }
 
             sys::cpu::set(1, sys::memory::MemoryBase::Application); // Probably
             break;
         case InfoType::AslrRegionSize:
-            if ((handle != KernelHandles::CurrentProcess) || (subType != 0)) {
+            if ((handle.raw != KernelHandles::CurrentProcess) || (subType != 0)) {
                 PLOG_WARNING << "Unexpected handle/sub type for AslrRegionSize";
             }
 
             sys::cpu::set(1, sys::memory::getAppSize()); // Probably
             break;
         case InfoType::StackRegionAddress:
-            if ((handle != KernelHandles::CurrentProcess) || (subType != 0)) {
+            if ((handle.raw != KernelHandles::CurrentProcess) || (subType != 0)) {
                 PLOG_WARNING << "Unexpected handle/sub type for StackRegionAddress";
             }
 
             sys::cpu::set(1, sys::memory::MemoryBase::Stack);
             break;
         case InfoType::StackRegionSize:
-            if ((handle != KernelHandles::CurrentProcess) || (subType != 0)) {
+            if ((handle.raw != KernelHandles::CurrentProcess) || (subType != 0)) {
                 PLOG_WARNING << "Unexpected handle/sub type for StackRegionSize";
             }
 
