f061702ff9cf4577d98b5a11fbba95eeafa2f360
Nomi <nomiwen@outlook.de>
2023-10-02 20:10:32 +0200

    Add HID service

---
+++ src/hle/service/hid.cpp
@@ -0,0 +1,84 @@
+/*
+    Nozomi is an experimental HLE Switch emulator.
+    Copyright (C) 2023  noumidev
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "hid.hpp"
+
+#include <ios>
+
+#include <plog/Log.h>
+
+#include "handle.hpp"
+#include "kernel.hpp"
+#include "result.hpp"
+
+namespace hle::service::hid {
+
+namespace Command {
+    enum : u32 {
+        CreateAppletResource,
+    };
+}
+
+Handle appletResource{.raw = 0ULL};
+
+void handleRequest(IPCContext &ctx, IPCContext &reply) {
+    const u32 command = ctx.getCommand();
+    switch (command) {
+        case Command::CreateAppletResource:
+            return cmdCreateAppletResource(ctx, reply);
+        default:
+            PLOG_FATAL << "Unimplemented command " << command;
+
+            exit(0);
+    }
+}
+
+void cmdCreateAppletResource(IPCContext &ctx, IPCContext &reply) {
+    const u32 *data = (u32 *)ctx.getData();
+
+    const u32 pid = data[0];
+    const u64 appletResourceUserID = (u64)data[0] | ((u64)data[1] << 32);
+
+    PLOG_INFO << "CreateAppletResource (PID = " << std::hex << pid << ", applet resource user ID = " << appletResourceUserID << ")";
+
+    if (appletResource.type == HandleType::None) {
+        appletResource = kernel::makeService<AppletResource>();
+    }
+
+    reply.makeReply(2, 0, 1);
+    reply.write(KernelResult::Success);
+    reply.moveHandle(appletResource);
+}
+
+AppletResource::AppletResource() {}
+
+AppletResource::~AppletResource() {}
+
+void AppletResource::handleRequest(IPCContext &ctx, IPCContext &reply) {
+    (void)reply;
+
+    const u32 command = ctx.getCommand();
+    switch (command) {
+        default:
+            PLOG_FATAL << "Unimplemented command " << command;
+
+            exit(0);
+    }
+}
+
+}
